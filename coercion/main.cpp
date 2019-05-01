ab5e68eeb524d324fcf575d911461a332f65da86
Florian Lautenschlager <florian.lautenschlager@qaware.de>
2019-05-02 00:09:36 +0200

    Add example about coercion

---
+++ coercion/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  coercion
+//
+//  Created by Florian Lautenschlager on 4/28/19.
+//  Copyright © 2019 Florian Lautenschlager. All rights reserved.
+//
+
+#include <iostream>
+#include <typeinfo>
+
+void print(int integer){
+    std::cout << "Argument is " << integer << " is of type " << typeid(integer).name() << std::endl;
+}
+
+int main(int argc, const char * argv[]) {
+    
+    print(5.3); //Implizit: Verengen (narrowing) double -> int
+    print(true); //Implizit: Erweitern (widening) bool -> int
+    print((int) false); //Explizit: Erweitern (narrowing) bool -> int
+    
+    return 0;
+}
